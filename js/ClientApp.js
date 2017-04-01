80eecc972dc00063f4520b7a814e1abc5034d24a
Enether <familyguyuser192@windowslive.com>
2017-04-01 16:26:55 +0300

    Added props

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,23 +1,26 @@
 
-
 // create a component class
 var MyTitle = React.createClass({
   render: function () {
     return (
       React.DOM.div(null,
-        React.DOM.h1(null, 'check this out bitches'))
+        React.DOM.h1(
+          { style: { color: this.props.color }},
+          this.props.title /* the prop we passed to it*/))
     )
   }
 })
+
+var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
       React.DOM.div(null,
-      // create multiple instances of MyTitle
-        React.createElement(MyTitle),
-        React.createElement(MyTitle),
-        React.createElement(MyTitle)
+        // create multiple instances of MyTitle, passing the title attribute to it
+        MyTitleFactory({ title: 'props are the best', color: 'peru'}),
+        MyTitleFactory({ title: 'semicolons are not the best', color: 'mediumaquamarine'}),
+        React.createElement(MyTitle, { title: 'props are the best', color: 'wheat'})
       )
     )
   }
